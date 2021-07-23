d9d7568134c78340ae07df5fbe6b2cdbe11f98b6
pedro21900@hotmail.com <pedro.lenonn@tre-pa.jus.br>
2021-07-23 14:35:13 -0300

    GraylogSendMensager

---
+++ src/main/java/Lab/grayLogsConnectionSpring/GrayLogsConnectionSpringApplication.java
@@ -0,0 +1,13 @@
+package Lab.grayLogsConnectionSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayLogsConnectionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrayLogsConnectionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lab/grayLogsConnectionSpring/SpringContextTest.java
@@ -0,0 +1,17 @@
+package Lab.grayLogsConnectionSpring;
+
+import Lab.baeldung.springbootlogging.SpringBootLoggingApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootLoggingApplication.class)
+class SpringContextTest {
+
+	@Test
+	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+	}
+
+}
